7c7d384597a89d56cd135840e7680a352b484675
Nicolas Saignes <nicolas@pointndots.com>
2017-02-20 22:20:01 +0100

    Extract App layout

---
+++ app/Resources/js/layout/component.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
+import Paper from 'material-ui/Paper';
+
+export const App = ({children}) => (
+    <MuiThemeProvider  muiTheme={getMuiTheme(darkBaseTheme)}>
+        <Paper zDepth={0} >
+            <AppBar title="Podcasts"/>
+            {children}
+        </Paper>
+    </MuiThemeProvider>
+)
