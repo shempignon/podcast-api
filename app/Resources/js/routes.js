1445ce4764943321c90f0d237f9d1fbe4be9e336
Nicolas Saignes <nicolas@pointndots.com>
2017-02-20 22:27:40 +0100

    Updated routes with new Layout and Podcast

--- app/Resources/js/routes.js
+++ app/Resources/js/routes.js
@@ -1,25 +1,21 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { Router, Route } from 'react-router'
+import { Router, Route, IndexRoute } from 'react-router'
 import { Podcasts } from './podcasts/index'
 import { store, history } from './store'
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import AppBar from 'material-ui/AppBar';
-import Paper from 'material-ui/Paper';
+import { App } from "./layout/component"
+import { Podcast } from "./podcast/index"
 
 const routes = () => (
-	<Provider store={store}>
-		<MuiThemeProvider  muiTheme={getMuiTheme(darkBaseTheme)}>
-			<Paper zDepth={0} style={{height: '100%', width: '100%' }}>
-				<AppBar />
-				<Router history={history}>
-					<Route path="/" component={Podcasts} />
-				</Router>
-			</Paper>
-		</MuiThemeProvider>
-	</Provider>
+    <Provider store={store}>
+        <Router history={history}>
+            <Route path="/" component={App} >
+                <IndexRoute component={Podcasts}/>
+                <Route path='/podcasts' component={Podcasts} />
+                <Route path='/podcast/:slug' component={Podcast} />
+            </Route>
+        </Router>
+    </Provider>
 )
 
 export default routes
