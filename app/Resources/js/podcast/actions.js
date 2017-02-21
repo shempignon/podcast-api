dff873128cec2b42dc024848fbc3e19051b02dba
Nicolas Saignes <nicolas@pointndots.com>
2017-02-21 02:04:34 +0100

    Refresh podcast when no episode are found

--- app/Resources/js/podcast/actions.js
+++ app/Resources/js/podcast/actions.js
@@ -3,17 +3,20 @@ export const key = 'podcast'
 
 // Action types
 export const PODCAST_SELECT = 'PODCASTS_FETCH'
+export const PODCAST_REFRESH = 'PODCAST_REFRESH'
 export const PODCAST_FETCH_FULFILLED = 'PODCAST_FETCH_FULFILLED'
 export const PODCAST_FETCH_REJECTED = 'PODCASTS_FETCH_REJECTED'
 
 export const actionTypes = {
     PODCAST_SELECT,
+	PODCAST_REFRESH,
     PODCAST_FETCH_FULFILLED,
     PODCAST_FETCH_REJECTED
 }
 
 // Action creators
 export const selectPodcast = slug => ({ type: PODCAST_SELECT, slug })
+export const refreshPodcast = (slug, name) => ({ type: PODCAST_REFRESH, slug, name })
 export const fetchFulfilled = payload => ({type: PODCAST_FETCH_FULFILLED, payload })
 export const fetchRejected = err => ({
     type: PODCAST_FETCH_REJECTED,
@@ -23,6 +26,7 @@ export const fetchRejected = err => ({
 
 export const actions = {
     selectPodcast,
+    refreshPodcast,
     fetchRejected,
     fetchFulfilled
 }
--- app/Resources/js/podcast/logic.js
+++ app/Resources/js/podcast/logic.js
@@ -1,44 +1,81 @@
 import { createLogic } from 'redux-logic'
 import {
     PODCAST_SELECT,
+    PODCAST_REFRESH,
     PODCAST_FETCH_FULFILLED,
     PODCAST_FETCH_REJECTED
 } from './actions'
 
 const podcastFetchLogic = createLogic({
     type: PODCAST_SELECT,
-    debounce: 500, // ms
-    latest: true, // take latest only
+    debounce: 500,
+    latest: true,
 
-    // let's prevent empty requests
     validate({ getState, action }, allow, reject) {
         if (action.slug) {
             allow(action)
-        } else { // empty request, silently reject
+        } else {
             reject()
         }
     },
 
-    // use axios injected as httpClient from configureStore logic deps
     process({ httpClient, getState, action }, dispatch, done) {
         httpClient.get(`/feeds/${action.slug}`)
-            .then(resp => resp.data) // use results prop of payload
-            .then(results => dispatch({
-                type: PODCAST_FETCH_FULFILLED,
-                payload: results
-            }))
+            .then(resp => resp.data)
+            .then(results => (!results.episodes.length ?
+				dispatch({
+					type: PODCAST_REFRESH,
+					slug: action.slug
+				}) :
+				dispatch({
+					type: PODCAST_FETCH_FULFILLED,
+					payload: results
+				})))
             .catch(err => {
-                console.error(err); // log since could be render err
+                console.error(err)
                 dispatch({
                     type: PODCAST_FETCH_REJECTED,
                     payload: err,
                     error: true
                 });
             })
-            .then(() => done()); // call done when finished dispatching
+            .then(() => done())
     }
 })
 
+const podcastRefreshLogic = createLogic({
+	type: PODCAST_REFRESH,
+	debounce: 500,
+	latest: true,
+
+	validate({ getState, action }, allow, reject) {
+		if (action.slug) {
+			allow(action)
+		} else {
+			reject()
+		}
+	},
+
+	process({ httpClient, getState, action }, dispatch, done) {
+		httpClient.get(`/feeds/${action.slug}/refresh`)
+			.then(resp => resp.data)
+			.then(results => dispatch({
+				type: PODCAST_SELECT,
+				slug: action.slug
+			}))
+			.catch(err => {
+				console.error(err)
+				dispatch({
+					type: PODCAST_FETCH_REJECTED,
+					payload: err,
+					error: true
+				});
+			})
+			.then(() => done())
+	}
+})
+
 export default [
-    podcastFetchLogic
+    podcastFetchLogic,
+	podcastRefreshLogic
 ]
