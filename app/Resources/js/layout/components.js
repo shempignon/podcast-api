d90b7e50fa92f383f8e449663bc8341f04a2950d
Nicolas Saignes <nicolas@pointndots.com>
2017-03-02 21:04:17 +0100

    Simplify Layout component

--- app/Resources/js/layout/components.js
+++ app/Resources/js/layout/components.js
@@ -15,29 +15,21 @@ const Menu = () => (
     </div>
 )
 
-class Layout extends Component {
-    constructor(props) {
-        super(props)
-    }
-
-    render() {
-        return (
-            <MuiThemeProvider>
-                <Paper zDepth={0}>
-                    <AppBar title={this.props.title}
-							onLeftIconButtonTouchTap={this.props.toggleDrawer}
-                    />
-					<Drawer children={<Menu />}
-							docked={false}
-							open={this.props.openDrawer}
-							onRequestChange={this.props.toggleDrawer}
-					/>
-                    {this.props.children}
-                </Paper>
-            </MuiThemeProvider>
-        )
-    }
-}
+const Layout = ({title, openDrawer, toggleDrawer, children}) => (
+    <MuiThemeProvider>
+        <Paper zDepth={0}>
+            <AppBar title={title}
+                    onLeftIconButtonTouchTap={toggleDrawer}
+            />
+            <Drawer children={<Menu />}
+                    docked={false}
+                    open={openDrawer}
+                    onRequestChange={toggleDrawer}
+            />
+            {children}
+        </Paper>
+    </MuiThemeProvider>
+)
 
 export default connect(
     state => ({
